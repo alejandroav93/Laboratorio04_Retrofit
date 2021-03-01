79cb378720f8d1e15649a217936ce2c9d70040b8
Alejandro Alvarez V <59927580+alejandroav93@users.noreply.github.com>
2021-03-01 12:30:40 -0600

    Updated Main class / Added apirest and JsonObjects

--- app/src/main/java/com/example/retrofit_pokemon/MainActivity.java
+++ app/src/main/java/com/example/retrofit_pokemon/MainActivity.java
@@ -3,7 +3,16 @@ package com.example.retrofit_pokemon;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.retrofit_pokemon.retrofit2.ApiRest;
+import com.google.gson.JsonObject;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,7 +23,26 @@ public class MainActivity extends AppCompatActivity {
         TextView btn_1 = findViewById(R.id.btn_1);
 
         btn_1.setOnClickListener(v -> {
+            ApiRest apiRest = new ApiRest();
+            Call<JsonObject> call = apiRest.getService().getNoticias();
 
+            call.enqueue(new Callback<JsonObject>() {
+                @Override
+                public void onResponse(Call<JsonObject> call, Response<JsonObject> response) {
+                    if(response.isSuccessful()){
+                        Log.i("information", "");
+                    }
+                    else{
+                        Toast.makeText(MainActivity.this,"correcto", Toast.LENGTH_LONG).show();
+                    }
+
+                }
+
+                @Override
+                public void onFailure(Call<JsonObject> call, Throwable t) {
+
+                }
+            });
         });
     }
 }
--- app/src/main/java/com/example/retrofit_pokemon/retrofit2/Urls.java
+++ app/src/main/java/com/example/retrofit_pokemon/retrofit2/Urls.java
@@ -1,20 +1,30 @@
 package com.example.retrofit_pokemon.retrofit2;
 
+import com.google.gson.JsonObject;
 
+import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.http.GET;
 
 public interface Urls {
 
-    @GET("/api/information")
-        void wsEmergencias(Callback<String> cb);
 
     @GET("/api/news")
+    Call<JsonObject> getNoticias();
+
+    @GET("/api/covid")
+    Call<JsonObject> getCovid();
+
+    @GET("/api/centros")
+    Call<JsonObject> getCentros();
+
+   /* @GET("/api/news")
     void wsNoticias(Callback<String> cb);
 
     @GET("/api/covid")
     void wsCovid(Callback<String> cb);
 
     @GET("/api/centros")
-    void wsCentros(Callback<String> cb);
+    void wsCentros(Callback<String> cb);*/
+
 }
