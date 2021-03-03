f65b4f438ca2839642ae77bc20e26beebf49f19f
Alejandro Alvarez V <59927580+alejandroav93@users.noreply.github.com>
2021-03-03 11:48:46 -0600

    Edited XML for better visualization

--- app/src/main/java/com/example/retrofit_pokemon/MainActivity.kt
+++ app/src/main/java/com/example/retrofit_pokemon/MainActivity.kt
@@ -16,43 +16,35 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val editUser: EditText
-        val btn_ingresar: TextView
-        val pokemon: TextView
-        btn_ingresar = findViewById(R.id.btn_ingresar)
-        editUser = findViewById(R.id.edit_user)
-        pokemon = findViewById(R.id.mostrar)
+        val btn_ingresar: TextView = findViewById(R.id.btn_ingresar)
+        val editUser: EditText = findViewById(R.id.edit_user)
+        val pokemon: TextView = findViewById(R.id.mostrar)
         btn_ingresar.setOnClickListener { v: View? ->
 
-            //------------------------- RETROFIT 2 -----------------------------------
             val retrofit2 = Retrofit2()
-            val call = retrofit2.service.getPokemonById(editUser.text.toString().toLowerCase())
-            if (call != null) {
-                call.enqueue(object : Callback<JsonObject?> {
-                    override fun onResponse(call: Call<JsonObject?>, response: Response<JsonObject?>) {
-                        if (response.isSuccessful) {
-                            val data: JsonObject?
-                            data = response.body()
-                            assert(data != null)
-                            val imprimirInfo = ("\n\nHabilidad: " +
-                                    data!!.getAsJsonArray("abilities")[1].asJsonObject
-                                            .getAsJsonObject("ability")["name"]
-                                    + "\nNombre: " + data["name"]
-                                    + "\nExperiencia Base: " + data["base_experience"] +
-                                    "\nAltura: " + data["height"] + " cm"
-                                    + "\nPeso: " + data["weight"] + " gramos")
-                            pokemon.text = imprimirInfo
-                            Log.i("detalle", data.toString())
-                        } else {
-                            Log.e("error", "Hubo un error inesperado!")
-                        }
+            retrofit2.service.getPokemonById(editUser.text.toString().toLowerCase())?.enqueue(object : Callback<JsonObject?> {
+                override fun onResponse(call: Call<JsonObject?>, response: Response<JsonObject?>) {
+                    if (response.isSuccessful) {
+                        val data: JsonObject? = response.body()
+                        assert(data != null)
+                        val imprimirInfo = ("\n\nHabilidad: " +
+                                data!!.getAsJsonArray("abilities")[1].asJsonObject
+                                        .getAsJsonObject("ability")["name"]
+                                + "\nNombre: " + data["name"]
+                                + "\nExperiencia Base: " + data["base_experience"] +
+                                "\nAltura: " + data["height"] + "0 cm"
+                                + "\nPeso: " + data["weight"] + " gramos")
+                        pokemon.text = imprimirInfo
+                        Log.i("detalle", data.toString())
+                    } else {
+                        Log.e("error", "Hubo un error inesperado!")
                     }
+                }
 
-                    override fun onFailure(call: Call<JsonObject?>, t: Throwable) {
-                        Log.e("error", t.toString())
-                    }
-                })
-            }
+                override fun onFailure(call: Call<JsonObject?>, t: Throwable) {
+                    Log.e("error", t.toString())
+                }
+            })
         }
     }
 }
